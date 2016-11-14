a36e4fe364e3a44ded0f3cb91e1f44f03961a71c
Matt Dionis <mattdionis@gmail.com>
2016-11-14 16:11:06 -0500

    Add Angular Material and Material Design icons

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 import { InMemoryWebApiModule } from 'angular-in-memory-web-api';
+import { MaterialModule } from '@angular/material';
 
 import { RecipesData } from './recipes-data';
 import { AppComponent } from './app.component';
@@ -15,7 +16,8 @@ import { AppComponent } from './app.component';
     BrowserModule,
     FormsModule,
     HttpModule,
-    InMemoryWebApiModule.forRoot(RecipesData)
+    InMemoryWebApiModule.forRoot(RecipesData),
+    MaterialModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
